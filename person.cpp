65efc93e505bc3192c16abd16bc3546701006aa4
EdwardLi98 <64086198+EdwardLi98@users.noreply.github.com>
2021-02-19 02:15:10 +1100

    Delete person.cpp
    Cleaning

--- person.cpp
+++
@@ -1,8 +0,0 @@
-#include "person.hpp"
-
-void blackJack::Person::showHand() {
-    std::cout << name_ << " has the following cards: " << std::endl;
-    for (auto card : *hand_) {
-        std::cout << card.getValue() << " of " << card.getSuit() << std::endl;
-    }
-}
