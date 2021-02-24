3997dad6e9f32c99126c290d39093c79c39b299e
Edward Li <edward.li190898@gmail.com>
2021-02-24 19:33:15 +1100

    Fix merge conflicts

--- include/game.hpp
+++ include/game.hpp
@@ -1,5 +1,5 @@
 #ifndef game_hpp
-#define game_hpp 
+#define game_hpp
 
 #define DEAL_LIMIT 16
 #define BLACKJACK 21
@@ -10,24 +10,27 @@
 #include <unordered_map>
 #include <functional>
 #include <range/v3/algorithm/for_each.hpp>
-#include <any>
 
-namespace blackJack {
+namespace blackJack
+{
 
-class Game {
+    class Game
+    {
     public:
         Game(Dealer dealer) : dealer_{dealer} {};
         void initialiseGame(); //Generate deck and add players
-        void runGame(); //Plays game
+        void runGame();        //Plays game
     private:
-        enum SystemCommand {
+        enum SystemCommand
+        {
             Add,
             Remove,
             Play,
             End,
             InvalidS
         };
-        enum PlayerCommand {
+        enum PlayerCommand
+        {
             Hit,
             Hold,
             Split,
@@ -35,20 +38,20 @@ class Game {
         };
         void setupGame();
         enum SystemCommand processSystemCommand(std::string input);
-        void executeSystemComamand(std::string input);
-        enum PlayerCommand processPlayerCommand(std::string input); 
-        void processTurn(Player& player); //TODO
-        void checkHand(Person& person); //Return the total value of a players hand
-        int activePlayers(); //Return the total number of players still able to process actions (those who haven't folded or busted)
-        int standingPlayers(); //Return the total number of players that are standing
+        void executeSystemCommand(std::string input);
+        enum PlayerCommand processPlayerCommand(std::string input);
+        void processTurn(Player &player); //TODO
+        void checkHand(Person &person);   //Return the total value of a players hand
+        int activePlayers();              //Return the total number of players still able to process actions (those who haven't folded or busted)
+        int standingPlayers();            //Return the total number of players that are standing
         void addPlayer(std::string name);
         void removePlayer(); //TDOO
-        void showPlayers(); //for debug purposes
+        void showPlayers();  //for debug purposes
         void setActiveGame(bool active) { activeGame_ = active; };
         std::vector<Player> players_;
         Dealer dealer_;
         bool activeGame_ = false; // True if a round is in progress and false if it has ended
-};
+    };
 
 }
 
--- source/game.cpp
+++ source/game.cpp
@@ -1,6 +1,7 @@
 #include "../include/game.hpp"
 
-void blackJack::Game::initialiseGame() {
+void blackJack::Game::initialiseGame()
+{
     //Assign a dealer to the game
     //std::cout << "Please enter player name: ";
     //std::string name;
@@ -8,21 +9,23 @@ void blackJack::Game::initialiseGame() {
     //addPlayer(name);
     //std::cout << name << " has been added to the game." << std::endl;
     //while (players_.size() > 0) {
-        //runGame();
+    //runGame();
     //}
     std::string input;
-    while (std::cin >> input) {
+    while (std::cin >> input)
+    {
         executeSystemCommand(input);
     }
 }
 
-void blackJack::Game::setupGame() {
+void blackJack::Game::setupGame()
+{
     // Resets the status for both players and dealer
     // Resets hand and redeals
     setActiveGame(true);
     dealer_.setup();
     std::cout << "=============== Starting Game ================" << std::endl;
-    ranges::for_each(players_, [this](Player& player) {
+    ranges::for_each(players_, [this](Player &player) {
         player.setStatus(Person::PlayerStatus::Playing);
         auto hand = player.getHand();
         hand->clear();
@@ -35,136 +38,170 @@ void blackJack::Game::setupGame() {
     dealer_.showHand();
 }
 
-void blackJack::Game::runGame() {
+void blackJack::Game::runGame()
+{
     setupGame();
-    while (activePlayers() > 0) {
+    while (activePlayers() > 0)
+    {
         // Loop through turns
-        ranges::for_each(players_, [this](Player& player) {
-            if (player.getStatus() == Person::PlayerStatus::Playing) {
+        ranges::for_each(players_, [this](Player &player) {
+            if (player.getStatus() == Person::PlayerStatus::Playing)
+            {
                 processTurn(player);
             }
         });
     }
     // Dealer needs to deal cards to themselves if there are players standing
-    if (standingPlayers() > 0) {
-        while (dealer_.sumHand() <= DEAL_LIMIT) {
+    if (standingPlayers() > 0)
+    {
+        while (dealer_.sumHand() <= DEAL_LIMIT)
+        {
             dealer_.deal(dealer_);
         }
         dealer_.showHand();
         checkHand(dealer_);
         auto dealerSum = dealer_.sumHand();
-        for (auto player: players_) {
-            if (player.getStatus() == Person::PlayerStatus::Busted) {
+        for (auto player : players_)
+        {
+            if (player.getStatus() == Person::PlayerStatus::Busted)
+            {
                 std::cout << player.getName() << " loses!" << std::endl;
             }
-            else if (dealer_.getStatus() == Person::PlayerStatus::Busted) {
+            else if (dealer_.getStatus() == Person::PlayerStatus::Busted)
+            {
                 std::cout << player.getName() << " wins!" << std::endl;
-            } 
-            else {
+            }
+            else
+            {
                 auto playerSum = player.sumHand();
-                if (playerSum > dealerSum) {
+                if (playerSum > dealerSum)
+                {
                     std::cout << player.getName() << " wins!" << std::endl;
                 }
-                else if (playerSum == dealerSum) {
+                else if (playerSum == dealerSum)
+                {
                     std::cout << player.getName() << " ties!" << std::endl;
                 }
-                else {
+                else
+                {
                     std::cout << player.getName() << " loses!" << std::endl;
                 }
             }
         }
     }
-    else {
+    else
+    {
         std::cout << "Dealer wins by deault!" << std::endl;
     }
     setActiveGame(false);
 }
 
-enum blackJack::Game::SystemCommand blackJack::Game::processSystemCommand(std::string input) {
-    if (input == "add") return Add;
-    else if (input == "remove") return SystemCommand::Remove;
-    else if (input == "play") return SystemCommand::Play;
-    else if (input == "end") return SystemCommand::End;
+enum blackJack::Game::SystemCommand blackJack::Game::processSystemCommand(std::string input)
+{
+    if (input == "add")
+        return Add;
+    else if (input == "remove")
+        return SystemCommand::Remove;
+    else if (input == "play")
+        return SystemCommand::Play;
+    else if (input == "end")
+        return SystemCommand::End;
     return SystemCommand::InvalidS;
 }
 
-void blackJack::Game::executeSystemComamand(std::string input) {
+void blackJack::Game::executeSystemCommand(std::string input)
+{
     auto command = processSystemCommand(input);
-    switch (command) {
-        case SystemCommand::Add:
-            break;
-        case SystemCommand::Remove:
-            break;
-        case SystemCommand::End:
-            break;
-        case SystemCommand::InvalidS:
-            std::cout << "Invalid command. Valid commands are: add [player], remove [player], end." << std::endl;
-            break;
+    switch (command)
+    {
+    case SystemCommand::Add:
+        break;
+    case SystemCommand::Remove:
+        break;
+    case SystemCommand::End:
+        break;
+    case SystemCommand::InvalidS:
+        std::cout << "Invalid command. Valid commands are: add [player], remove [player], end." << std::endl;
+        break;
     }
 }
 
-enum blackJack::Game::PlayerCommand blackJack::Game::processPlayerCommand(std::string input) {
-    if (input == "hit") return PlayerCommand::Hit;
-    if (input == "hold") return PlayerCommand::Hold;
+enum blackJack::Game::PlayerCommand blackJack::Game::processPlayerCommand(std::string input)
+{
+    if (input == "hit")
+        return PlayerCommand::Hit;
+    if (input == "hold")
+        return PlayerCommand::Hold;
     return PlayerCommand::InvalidP;
 }
 
-void blackJack::Game::processTurn(Player& player) {
+void blackJack::Game::processTurn(Player &player)
+{
     std::string input;
     std::cout << "What would " << player.getName() << " like to do?" << std::endl;
     std::cin >> input;
     auto command = processPlayerCommand(input);
-    switch (command) {
-        case PlayerCommand::Hit:
-            //std::cout << player.getName() << " hits." << std::endl;
-            player.hit(dealer_);
-            player.showHand();
-            checkHand(player);
-            break;
-        case PlayerCommand::Hold:
-            //std::cout << player.getName() << " holds." << std::endl;
-            player.setStatus(Person::PlayerStatus::Standing);
-            break;
-        case PlayerCommand::InvalidP:
-            std::cout << "Invalid command. Valid commands are: hit, hold, split." << std::endl;
-            break;
+    switch (command)
+    {
+    case PlayerCommand::Hit:
+        //std::cout << player.getName() << " hits." << std::endl;
+        player.hit(dealer_);
+        player.showHand();
+        checkHand(player);
+        break;
+    case PlayerCommand::Hold:
+        //std::cout << player.getName() << " holds." << std::endl;
+        player.setStatus(Person::PlayerStatus::Standing);
+        break;
+    case PlayerCommand::InvalidP:
+        std::cout << "Invalid command. Valid commands are: hit, hold, split." << std::endl;
+        break;
     }
 }
 
-int blackJack::Game::standingPlayers() {
-    auto num = ranges::accumulate(players_ | 
-               ranges::view::transform([](Player player) { return player.getStatus() == Person::PlayerStatus::Standing ? 1 : 0; }), 0);
+int blackJack::Game::standingPlayers()
+{
+    auto num = ranges::accumulate(players_ |
+                                      ranges::view::transform([](Player player) { return player.getStatus() == Person::PlayerStatus::Standing ? 1 : 0; }),
+                                  0);
     return num;
 }
 
-int blackJack::Game::activePlayers() {
-    auto num = ranges::accumulate(players_ | 
-               ranges::view::transform([](Player player) { return player.getStatus() == Person::PlayerStatus::Playing ? 1 : 0; }), 0);
+int blackJack::Game::activePlayers()
+{
+    auto num = ranges::accumulate(players_ |
+                                      ranges::view::transform([](Player player) { return player.getStatus() == Person::PlayerStatus::Playing ? 1 : 0; }),
+                                  0);
     return num;
 }
 
-void blackJack::Game::checkHand(Person& person) {
+void blackJack::Game::checkHand(Person &person)
+{
     auto sum = person.sumHand();
-    if (sum == BLACKJACK) {
+    if (sum == BLACKJACK)
+    {
         std::cout << person.getName() << " has a blackjack!" << std::endl;
         person.setStatus(Person::PlayerStatus::BlackJack);
     }
-    else if (sum > BLACKJACK) {
+    else if (sum > BLACKJACK)
+    {
         std::cout << person.getName() << " has busted!" << std::endl;
         person.setStatus(Person::PlayerStatus::Busted);
     }
 }
 
-
-void blackJack::Game::addPlayer(std::string name) {
+void blackJack::Game::addPlayer(std::string name)
+{
     auto player = Player(name);
     players_.push_back(player);
 }
 
-void blackJack::Game::showPlayers() {
+void blackJack::Game::showPlayers()
+{
     std::cout << "Dealer for this game is: " << dealer_.getName() << std::endl;
     std::cout << "Players in the game are: " << std::endl;
-    for (auto player : players_) {
+    for (auto player : players_)
+    {
         std::cout << player.getName() << std::endl;
     }
 }
